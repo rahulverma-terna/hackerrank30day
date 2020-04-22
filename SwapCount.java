3579b63dced858ab1c062fb2124cc545aa030359
rahulverma-terna <31897480+rahulverma-terna@users.noreply.github.com>
2020-04-22 17:31:45 +0530

    Update and rename Solution.java to SwapCount.java

--- SwapCount.java
+++ SwapCount.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SwapCount {
 
     // Complete the countSwaps function below.
     static void countSwaps(int[] a) {
